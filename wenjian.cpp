ba9634075eb343432178e2ba5f021b5c5b96e23a
gd5512 <35511293+gd5512@users.noreply.github.com>
2018-03-30 21:37:25 +0800

    Add files via upload

---
+++ wenjian.cpp
@@ -0,0 +1,19 @@
+#include<fstream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+  int i,j,k;
+  ofstream ofile;//定义输出文件
+  ofile.open("d:\\myfile.txt");//输出文件打开
+  ofile<<"  公鸡   母鸡   小鸡"<<endl;
+  for(i=0;i<=20;i++)
+	  for(j=0;j<=33;j++)
+	  {
+	    k=100-i-j;
+		if((5*i+3*j+k/3==100)&&(k%3==0))
+			ofile<<setw(6)<<i<<setw(10)<<j<<setw(10)<<k<<endl;//数据写入文件
+	  }
+	  ofile.close();
+ return 0;
+}
