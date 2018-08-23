9e66427e734ce1c1a86b2c8e524e9820940b25a8
gd5512 <35511293+gd5512@users.noreply.github.com>
2018-08-23 17:02:21 +0800

    Add files via upload

--- data_struct/paixu/d.cpp
+++ data_struct/paixu/d.cpp
@@ -2,46 +2,61 @@
 //将距离某个增量的子序列分割后，分别进行直接插入排序排序。
 #include <iostream>
 using namespace std;
-void ShellSort(int array[], int n)
-{
-	int i, j;
-	int increment = n;
-	do
-	{
-		increment = increment / 3 + 1;//增量序列
-		for (i = increment + 1; i <= n; i++)
-		{
-			if (array[i] < array[i - increment])
-			{
-				array[0] = array[i];
-				for (j = i - increment; j>0 && array[0] < array[j]; j -= increment)
-					array[j + increment] = array[j];
-				array[j + increment] = array[0];
-			}
-		}
-	} 
-	while (increment>1);
-}
+void MaxHeapFixDown(int a[], int i, int n);
+void HeapSort(int a[], int n);
+
 void main()
 {
-	int arr[10],i;
-	cout << "请输入要排序的元素：" << endl;
+	int i;
+	int arr[10]={50,10,90,30,70,40,80,60,20,100};
+	//cout << "请输入要排序的元素：" << endl;
+	cout<<"原始数据："<<endl;
 	for ( i = 0; i < 10; i++)
 	{
-		cin >> arr[i];
-		//cout << "排序前的无序数组元素:" << arr[i] << ' ';
+		cout <<arr[i]<<" ";
+	
 	}
-	cout<<endl;
+    cout<<endl;
 
-	ShellSort(arr, 10);
+	HeapSort(arr, 10);
 	cout << "排序后的数组元素:" << endl;
 	for ( i = 0; i < 10; i++)
 	{
 		cout << arr[i] << ' ';
 	}
 	cout<<endl;
-	system("pause");
+//	system("pause");
 }
 
 
-//注意：arr[0]是哨兵，不参与排序
+
+
+
+//构造最大堆
+void MaxHeapFixDown(int a[], int i, int n){
+    int j = 2*i+1;
+    int temp = a[i];
+    while(j<n){
+        if(j+1<n&&a[j]<a[j+1])
+            ++j;
+        if(temp>a[j])
+            break;
+        else{
+            a[i]=a[j];
+            i=j;
+            j=2*i+1;
+        }
+    }
+    a[i]=temp;
+}
+
+//堆排序
+void HeapSort(int a[], int n){
+	int i;
+    for( i= n/2-1;i>=0;i--)
+        MaxHeapFixDown(a,i,n);
+    for( i=n-1;i>=1;i--){
+        swap(a[i],a[0]);
+        MaxHeapFixDown(a,0,i);
+    }
+}
